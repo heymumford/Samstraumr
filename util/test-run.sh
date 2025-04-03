8799fea828d7da062d48d3986f6948d3e84f249b
Eric Mumford <ericmumford@outlook.com>
2025-04-03 16:58:18 -0400

    Fix script paths for test-run.sh and run-tests.sh
    - Update util/test-run.sh to properly reference test-run-all.sh
    - Ensure backward compatibility with redirects
    - Test script functionality by running smoke tests

--- util/test-run.sh
+++ util/test-run.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# Legacy wrapper - redirects to the new location
+# Main test runner script
 
-echo -e "\033[1;33mWARNING: run-tests.sh has been moved to util/test/\033[0m"
-echo -e "Please use \033[1;32m./util/test/run-tests.sh\033[0m or \033[1;32m./util/test/run-all-tests.sh\033[0m instead."
-echo ""
+# Default to running all tests if no argument is provided
+TEST_TYPE=${1:-all}
 
-# Forward to the appropriate script
-if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-  echo -e "\033[1;32mShowing help from ./util/test/run-all-tests.sh:\033[0m"
-  ./util/test/run-all-tests.sh --help
-else
-  ./util/test/run-all-tests.sh "$@"
-fi
+# Forward to the appropriate script based on the argument
+echo -e "\033[0;34mRunning tests: ${TEST_TYPE}\033[0m"
+
+# Use the test-run-all.sh script
+./util/test-run-all.sh "$@"
